a300e61637d7d7cdb626cd4940a8e6d83748bcf3
shawiizz <shaweereso@gmail.com>
2022-04-03 16:52:53 +0200

    oups

--- server/util/Logger.js
+++ server/util/Logger.js
@@ -8,6 +8,6 @@ export function warn(s) {
     console.log(`\u001b[1;33m[Menu-LLA-Story | ${getDateNow()}] [WARN]\u001b[0m`, s);
 }
 
-export function errlog(...s) {
+export function errlog(s) {
     console.log(`\u001b[1;31m[Menu-LLA-Story | ${getDateNow()}] [ERROR]\u001b[0m`, s);
 }
